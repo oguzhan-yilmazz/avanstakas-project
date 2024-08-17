03970e08d03bb513d1776264602e2192f7aad773
oguzhan-yilmazz <ous2ylmz@gmail.com>
2024-08-18 00:12:55 +0300

    initial commit

---
+++ AvansTakas/avanstakas_bacend/avanstakas/src/main/java/com/project/avanstakas/AvanstakasApplication.java
@@ -0,0 +1,13 @@
+package com.project.avanstakas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanstakasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanstakasApplication.class, args); 
+	}
+
+}
---
+++ AvansTakas/avanstakas_bacend/avanstakas/src/test/java/com/project/avanstakas/AvanstakasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.avanstakas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanstakasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
